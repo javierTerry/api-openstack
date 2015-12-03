e3960123c3fc1f55aa0e52e68130b5ab6b9d9ba4
Your Name <you@example.com>
2015-12-03 17:10:54 -0600

    Segagre el cliente nova

---
+++ nova/nova_v11.1.py
@@ -0,0 +1,56 @@
+import os
+
+import novaclient.v1_1.client as nvclient
+#import novaclient.v3.client as nvclient
+import json
+# Read from the env vars
+# TODO parse arguments in command line
+def get_nova_credentials( sType):
+    cred = {}
+    cred['username']   = os.environ['OS_USERNAME']
+    cred['api_key']    = os.environ['OS_PASSWORD']
+    cred['auth_url']   = os.environ['OS_AUTH_URL']
+    cred['project_id'] = os.environ['OS_PROJECT_NAME']# "IMSS-1" #"UNETE_Op-07678" #"SORIANA_Op-08151"#os.environ['OS_TENANT_NAME']
+    if sType == "volume" :
+	cred['service_type'] = "volume"
+    return cred
+
+def main():
+    credentials = get_nova_credentials("")
+    print credentials
+    nc = nvclient.Client(**credentials)
+    #print nc.volumes.list()
+    
+    #for volume in nc.volumes.list():
+#	print "Valumen" , repr(volume)
+# 	print volume.size
+
+
+    #for volume in nc.volumes.get_server_volumes("1af9e615-68c4-48e9-bb8e-1ad8e24ece40"):
+#	 print "Volume in server" , repr(volume)    
+    for server in nc.servers.list():
+	print server.name
+	print "UUID instance "
+	print server.id
+	#print nc.volumes.get_server_volumes(server.id)
+	credentials = get_nova_credentials("volume")
+   	#nc = nvclient.Client(**credentials)
+
+	for volume in nc.volumes.get_server_volumes(server.id):
+		print "Volume ->", repr(volume.volumeId)
+		print dir(volume) 
+		#print "Volume get->", repr(nc.voluddmes.get("31446692-49dd-4985-ab3c-add26ad83a44"))
+		#print "Volume get->", repr(volume.id)
+		#print "Tamano :", repr(volume.size)
+	#print server.image
+
+#    for flavor in nc.flavors.list():
+#	print flavor
+#	repr(flavor)
+	#json.dumps(flavor, sort_keys=True, indent=4)
+	#print flavor.ram
+
+
+
+if __name__ == '__main__':
+    main()
