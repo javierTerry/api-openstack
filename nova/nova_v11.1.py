a9eeb3c6769dc277ad07d7eb4a3fe0857dafb75b
Your Name <you@example.com>
2015-12-07 17:48:55 -0600

    Actualizacion de pruebas de api

--- nova/nova_v11.1.py
+++ nova/nova_v11.1.py
@@ -10,40 +10,43 @@ def get_nova_credentials( sType):
     cred['username']   = os.environ['OS_USERNAME']
     cred['api_key']    = os.environ['OS_PASSWORD']
     cred['auth_url']   = os.environ['OS_AUTH_URL']
-    cred['project_id'] = os.environ['OS_PROJECT_NAME']# "IMSS-1" #"UNETE_Op-07678" #"SORIANA_Op-08151"#os.environ['OS_TENANT_NAME']
-    if sType == "volume" :
-	cred['service_type'] = "volume"
+    cred['project_id'] = "IMSS-1"#os.environ['OS_PROJECT_NAME']# "IMSS-1" #"UNETE_Op-07678" #"SORIANA_Op-08151"#os.environ['OS_TENANT_NAME']
+    #cred['service_type'] = "volume"
     return cred
 
 def main():
     credentials = get_nova_credentials("")
     print credentials
     nc = nvclient.Client(**credentials)
-    #print nc.volumes.list()
-    
+    #print nc.volumes.list(False)
+    #volumes = nc.volumes.list()
+    #print dir(volumes[0])
+    #print nc.servers.list()   
+
+    #exit();
     #for volume in nc.volumes.list():
-#	print "Valumen" , repr(volume)
-# 	print volume.size
+	#print "Valumen" , repr(volume)
+ 	#volume.size
 
-
-    #for volume in nc.volumes.get_server_volumes("1af9e615-68c4-48e9-bb8e-1ad8e24ece40"):
+    #exit();
+    #print nc.flavors.list()
+    #print nc.servers.list()
+    #print nc.volumes.list()
+	#for volume in nc.volumes.get_server_volumes("1af9e615-68c4-48e9-bb8e-1ad8e24ece40"):
 #	 print "Volume in server" , repr(volume)    
     for server in nc.servers.list():
-	print server.name
-	print "UUID instance "
-	print server.id
-	#print nc.volumes.get_server_volumes(server.id)
-	credentials = get_nova_credentials("volume")
-   	#nc = nvclient.Client(**credentials)
-
+	#print "VM id= {}, name = {}".format(server.id,server.name)
+        attach = ""
 	for volume in nc.volumes.get_server_volumes(server.id):
-		print "Volume ->", repr(volume.volumeId)
-		print dir(volume) 
+		#print "Volume ->", repr(volume.volumeId)
+		print volume.id
+		attach = volume.id + "," + attach
+		#print volume.human_id
 		#print "Volume get->", repr(nc.voluddmes.get("31446692-49dd-4985-ab3c-add26ad83a44"))
 		#print "Volume get->", repr(volume.id)
 		#print "Tamano :", repr(volume.size)
-	#print server.image
-
+	
+	print "VM id= {}, name = {}, attach = {}".format(server.id,server.name,attach)
 #    for flavor in nc.flavors.list():
 #	print flavor
 #	repr(flavor)
