86f25fba4e61cd63da2699f9bb85c923301b7b8c
Your Name <you@example.com>
2015-12-07 18:38:01 -0600

    REnombre y se agrega un ignore local

--- nova/server.py
+++ nova/server.py
@@ -3,6 +3,7 @@ import os
 import novaclient.v1_1.client as nvclient
 #import novaclient.v3.client as nvclient
 import json
+import csv
 # Read from the env vars
 # TODO parse arguments in command line
 def get_nova_credentials( sType):
@@ -18,11 +19,6 @@ def main():
     credentials = get_nova_credentials("")
     print credentials
     nc = nvclient.Client(**credentials)
-    #print nc.volumes.list(False)
-    #volumes = nc.volumes.list()
-    #print dir(volumes[0])
-    #print nc.servers.list()   
-
     #exit();
     #for volume in nc.volumes.list():
 	#print "Valumen" , repr(volume)
@@ -33,7 +29,12 @@ def main():
     #print nc.servers.list()
     #print nc.volumes.list()
 	#for volume in nc.volumes.get_server_volumes("1af9e615-68c4-48e9-bb8e-1ad8e24ece40"):
-#	 print "Volume in server" , repr(volume)    
+#	 print "Volume in server" , repr(volume) 
+    #f = open('test.json')
+    #data = json.load(f)
+    #f.close()
+
+    
     for server in nc.servers.list():
 	#print "VM id= {}, name = {}".format(server.id,server.name)
         attach = ""
@@ -47,12 +48,32 @@ def main():
 		#print "Tamano :", repr(volume.size)
 	
 	print "VM id= {}, name = {}, attach = {}".format(server.id,server.name,attach)
+     	data = {}
+	data['uuid'] = server.id
+	data['name'] = server.name
+	json_data = json.dumps(data)
+	print json_data
+	write(json_data)
+     
 #    for flavor in nc.flavors.list():
 #	print flavor
 #	repr(flavor)
 	#json.dumps(flavor, sort_keys=True, indent=4)
 	#print flavor.ram
 
+def write(cadena):
+    print('Creating new text file') 
+
+    name = "test.txt" #raw_input('Enter name of text file: ')+'.txt'  # Name of text file coerced with +.txt
+
+    try:
+        file = open(name,'w')   # Trying to create a new file or open one
+        file.write(cadena)
+	file.close()
+
+    except:
+        print('Something went wrong! Can\'t tell what?')
+        sys.exit(0) # quit Python
 
 
 if __name__ == '__main__':
